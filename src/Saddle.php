c32647822e720a374d8ee7da2fbe6f06e7a2b25c
Matt Gros <mpge@users.noreply.github.com>
2026-07-19 20:48:50 -0400

    chore(release): v1.1.0

--- src/Saddle.php
+++ src/Saddle.php
@@ -15,7 +15,7 @@ use SaddlePHP\Widgets\Widget;
 
 class Saddle
 {
-    public const VERSION = '1.0.1';
+    public const VERSION = '1.1.0';
 
     /** @var array<int, class-string<\SaddlePHP\Resource>> */
     protected array $registered = [];
